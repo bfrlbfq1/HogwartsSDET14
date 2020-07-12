145906e42d20d2d462527bd5aae035837777db66
root <21179699066@qq.com>
2020-07-12 20:17:46 +0800

    测试开发14期 第三次作业

--- testing/test_calc.py
+++ testing/test_calc.py
@@ -11,21 +11,21 @@ import yaml
 
 class TestCalc:
     @pytest.mark.parametrize('a,b,result',
-                             yaml.safe_load(open('D:\\Studies\\ceshiren\\HogwartsSdet14\\yamlData/addyaml_1.yaml')))
+                             yaml.safe_load(open('./addyaml_1.yaml')))
     def test_add(self, setup_teardown_calc, a, b, result):
         assert result == setup_teardown_calc.add(a, b)
 
     @pytest.mark.parametrize('a,b,result',
-                             yaml.safe_load(open('D:\\Studies\\ceshiren\\HogwartsSdet14\\yamlData/addyaml_2.yaml')))
+                             yaml.safe_load(open('./addyaml_2.yaml')))
     def test_div(self, setup_teardown_calc, a, b, result):
         assert result == setup_teardown_calc.div(a, b)
 
     @pytest.mark.parametrize('a,b,result',
-                             yaml.safe_load(open('D:\\Studies\\ceshiren\\HogwartsSdet14\\yamlData/addyaml_3.yaml')))
+                             yaml.safe_load(open('./addyaml_3.yaml')))
     def test_sub(self, setup_teardown_calc, a, b, result):
         assert result == setup_teardown_calc.sub(a, b)
 
     @pytest.mark.parametrize('a,b,result',
-                             yaml.safe_load(open('D:\\Studies\\ceshiren\\HogwartsSdet14\\yamlData/addyaml_4.yaml')))
+                             yaml.safe_load(open('./addyaml_4.yaml')))
     def test_ride(self, setup_teardown_calc, a, b, result):
         assert result == setup_teardown_calc.ride(a, b)
